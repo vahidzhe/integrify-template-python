5d961cc917c4fbd0b0eb45bf71274acf5dc573e9
mmzeynalli <miradil.zeynalli@gmail.com>
2025-08-12 00:16:38 +0200

    Fixed create_local_configs script

--- create_local_configs.py
+++ create_local_configs.py
@@ -7,22 +7,14 @@ def promote_example_files(root_dir='.'):
 
     shutil.move('README.md', 'Template-README.md')
 
-    for example_file in root.rglob('*.example.*'):
+    for example_file in root.rglob('*.example*'):
         target_file = example_file.with_name(example_file.name.replace('.example', ''))
 
         if target_file.exists():
             print(f'🟡 Skipped (already exists): {target_file}')  # noqa: T201
         else:
-            shutil.copy(example_file, target_file)
-            print(f'✅ Created: {target_file}')  # noqa: T201
-
-        # Remove the example file
-        if example_file.is_dir():
-            example_file.rmdir()
-        else:
-            example_file.unlink()
-
-        print(f'🗑️  Removed: {example_file}')  # noqa: T201
+            shutil.move(example_file, target_file)
+            print(f'✅ Renamed: {target_file}')  # noqa: T201
 
 
 if __name__ == '__main__':
